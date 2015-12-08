90fdd7321b4a325b9db03214f85630d5df8d8f56
lei7dover <limhoff@wastebits.com>
2015-12-08 16:57:03 -0500

    forms views

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,2 +1,28 @@
 class UserSessionsController < ApplicationController
+  before_action :require_user, only:[:destroy]
+
+  def new
+    @user_session = UserSession.new
+  end
+
+  def create
+    @user_session = UserSession.new(params[:user_session])
+    if @user_session.save
+      respond_to do |format|
+        format.html
+        format.js { }
+      end
+    else
+      respond_to do |format|
+          format.html
+          format.js {render 'errors'}
+        end
+    end
+  end
+
+  def destroy
+    current_user_session.destroy
+    flash[:alert] = "Logout successful!"
+    redirect_to root_url
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,54 @@
 class UsersController < ApplicationController
+before_filter :require_user, :only => [ :edit, :update]
+ before_filter :set_user, only: [:edit, :update]
+
+ def index
+   @users=User.all
+ end
+
+ def new
+   @user = User.new
+ end
+
+ def create
+   @user = User.new(user_params)
+   if @user.save
+     respond_to do |format|
+       format.html { redirect_to @user, notice: 'User was successfully created.' }
+       format.js { }
+     end
+   else
+     respond_to do |format|
+       format.js {render 'errors'}
+     end
+   end
+ end
+
+ def edit
+   @user = @current_user
+ end
+
+ def update
+ @user = @current_user # makes our views "cleaner" and more consistent
+   respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to root_path, notice: 'User was successfully updated.' }
+      
+      else
+        format.html { redirect_to root_path }
+        format.json { render js: @user.errors, status: :unprocessable_entity }
+      end
+    end
+ end
+
+ private
+
+  def set_user
+    @user=User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email,:name, :password, :password_confirmation)
+  end
+
 end
