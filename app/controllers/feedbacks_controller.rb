ce8c57ac1829edc45ce8997a7d157eb595f6a45b
angiemartaus <angelamartaus@gmail.com>
2015-12-08 16:11:07 -0500

    Form for feedback, still need to add js

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,2 +1,21 @@
 class FeedbacksController < ApplicationController
+
+  def create
+    @feedback = Feedback.new(feedback_params)
+    @feedback.user = current_user
+    if @feedback.save
+      redirect_to root_path
+    else
+      respond_to do |format|
+        format.js {render partial: "shared/feedback_form"}
+      end
+    end
+  end
+
+  private
+
+  def feedback_params
+    params.require(:feedback).permit(:body)
+  end
+
 end
