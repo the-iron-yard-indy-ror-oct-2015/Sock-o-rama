69d599a4880a3e83ded4c5df6a97264da001e539
Matt Fair <matt.j.fair@gmail.com>
2015-12-08 19:23:38 -0500

    delete

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,23 @@
 class CartsController < ApplicationController
 
-def index
-end
+  def index
+    @items = Itme.all
+  end
+
+  def create
+    session['cart_id'] = SecureRandom.hex.to_s+created_at.strftime("%Y%m%d%H%M%S")
+    @cart = Cart.new()
+    if current_user_session
+      @cart.user = current_user
+    else
+      @cart.user = session['cart_id']
+    end
+    @cart.save
+  end
+
+  def destroy
+
+  end
 
 
 
