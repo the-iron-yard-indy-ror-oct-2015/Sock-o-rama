b6ddb32b69878e22806bbba7e186581cdca18db5
lei7dover <limhoff@wastebits.com>
2015-12-09 13:34:08 -0500

    added user create test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,10 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "user_create" do
+    user = create(:user)
+    assert user.save, "User was not created"
+  end
+
 end
