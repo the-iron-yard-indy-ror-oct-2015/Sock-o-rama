5e529175cfc4745951da1f91b6cccf9bea55b105
Matt Fair <matt.j.fair@gmail.com>
2015-12-09 21:38:47 -0500

    payola start

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209195435) do
+ActiveRecord::Schema.define(version: 20151210022535) do
 
   create_table "carts", force: :cascade do |t|
     t.integer  "sock_id"
     t.integer  "user_id"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.float    "total",      default: 0.0
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "total",      default: 0
   end
 
   create_table "feedbacks", force: :cascade do |t|
@@ -28,6 +28,19 @@ ActiveRecord::Schema.define(version: 20151209195435) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+
   create_table "items", force: :cascade do |t|
     t.integer  "sock_id"
     t.integer  "cart_id"
@@ -35,104 +48,6 @@ ActiveRecord::Schema.define(version: 20151209195435) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "payola_affiliates", force: :cascade do |t|
-    t.string   "code"
-    t.string   "email"
-    t.integer  "percent"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "payola_coupons", force: :cascade do |t|
-    t.string   "code"
-    t.integer  "percent_off"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "active",      default: true
-  end
-
-  create_table "payola_sales", force: :cascade do |t|
-    t.string   "email",                limit: 191
-    t.string   "guid",                 limit: 191
-    t.integer  "product_id"
-    t.string   "product_type",         limit: 100
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "state"
-    t.string   "stripe_id"
-    t.string   "stripe_token"
-    t.string   "card_last4"
-    t.date     "card_expiration"
-    t.string   "card_type"
-    t.text     "error"
-    t.integer  "amount"
-    t.integer  "fee_amount"
-    t.integer  "coupon_id"
-    t.boolean  "opt_in"
-    t.integer  "download_count"
-    t.integer  "affiliate_id"
-    t.text     "customer_address"
-    t.text     "business_address"
-    t.string   "stripe_customer_id",   limit: 191
-    t.string   "currency"
-    t.text     "signed_custom_fields"
-    t.integer  "owner_id"
-    t.string   "owner_type",           limit: 100
-  end
-
-  add_index "payola_sales", ["coupon_id"], name: "index_payola_sales_on_coupon_id"
-  add_index "payola_sales", ["email"], name: "index_payola_sales_on_email"
-  add_index "payola_sales", ["guid"], name: "index_payola_sales_on_guid"
-  add_index "payola_sales", ["owner_id", "owner_type"], name: "index_payola_sales_on_owner_id_and_owner_type"
-  add_index "payola_sales", ["product_id", "product_type"], name: "index_payola_sales_on_product"
-  add_index "payola_sales", ["stripe_customer_id"], name: "index_payola_sales_on_stripe_customer_id"
-
-  create_table "payola_stripe_webhooks", force: :cascade do |t|
-    t.string   "stripe_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "payola_subscriptions", force: :cascade do |t|
-    t.string   "plan_type"
-    t.integer  "plan_id"
-    t.datetime "start"
-    t.string   "status"
-    t.string   "owner_type"
-    t.integer  "owner_id"
-    t.string   "stripe_customer_id"
-    t.boolean  "cancel_at_period_end"
-    t.datetime "current_period_start"
-    t.datetime "current_period_end"
-    t.datetime "ended_at"
-    t.datetime "trial_start"
-    t.datetime "trial_end"
-    t.datetime "canceled_at"
-    t.integer  "quantity"
-    t.string   "stripe_id"
-    t.string   "stripe_token"
-    t.string   "card_last4"
-    t.date     "card_expiration"
-    t.string   "card_type"
-    t.text     "error"
-    t.string   "state"
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "currency"
-    t.integer  "amount"
-    t.string   "guid",                 limit: 191
-    t.string   "stripe_status"
-    t.integer  "affiliate_id"
-    t.string   "coupon"
-    t.text     "signed_custom_fields"
-    t.text     "customer_address"
-    t.text     "business_address"
-    t.integer  "setup_fee"
-  end
-
-  add_index "payola_subscriptions", ["guid"], name: "index_payola_subscriptions_on_guid"
 
   create_table "socks", force: :cascade do |t|
     t.string   "size"
@@ -145,6 +60,7 @@ ActiveRecord::Schema.define(version: 20151209195435) do
     t.datetime "updated_at", null: false
     t.string   "img_url"
     t.string   "name"
+    t.string   "permalink"
   end
 
   create_table "subscription_plans", force: :cascade do |t|
